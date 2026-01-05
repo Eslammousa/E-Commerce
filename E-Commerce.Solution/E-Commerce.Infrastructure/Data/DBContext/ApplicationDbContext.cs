466405bd169609f4f93277eb8c38808cbad156ec
Eslam Mousa <EslamMousa123@gmail.com>
2026-01-05 12:50:35 +0200

    Add DbContext

---
+++ E-Commerce.Solution/E-Commerce.Infrastructure/Data/DBContext/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using E_Commerce.Core.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_Commerce.Infrastructure.Data.DBContext
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public class AppDbContext : DbContext
+        {
+            public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+            public DbSet<Product> products { get; set; }
+            public DbSet<Category> categories { get; set; }
+
+            protected override void OnModelCreating(ModelBuilder modelBuilder)
+            {
+                base.OnModelCreating(modelBuilder);
+                modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+
+            }
+
+        }
+    }
+}
