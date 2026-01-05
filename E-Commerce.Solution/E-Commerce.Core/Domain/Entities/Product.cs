7e27d1773785f865cc5b806a1e1634007b10ee02
Eslam Mousa <EslamMousa123@gmail.com>
2026-01-05 12:40:00 +0200

    Add Entity

---
+++ E-Commerce.Solution/E-Commerce.Core/Domain/Entities/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace E_Commerce.Core.Domain.Entities
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public int StockQuantity { get; set; }
+       public string ImageUrl { get; set; } = string.Empty;
+        public Guid CategoryId { get; set; }
+        public Category Category { get; set; } = null!;
+    }
+}
