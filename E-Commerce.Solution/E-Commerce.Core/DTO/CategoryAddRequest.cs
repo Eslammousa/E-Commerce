4cff5171e31549d056a59f92587079423c5235d4
Eslam Mousa <EslamMousa123@gmail.com>
2026-01-06 09:36:30 +0200

    Add Category DTO

---
+++ E-Commerce.Solution/E-Commerce.Core/DTO/CategoryAddRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace E_Commerce.Core.DTO
+{
+    public class CategoryAddRequest
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+    }
+}
---
+++ E-Commerce.Solution/E-Commerce.Core/DTO/CategoryResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace E_Commerce.Core.DTO
+{
+    public class CategoryResponse
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+    }
+}
