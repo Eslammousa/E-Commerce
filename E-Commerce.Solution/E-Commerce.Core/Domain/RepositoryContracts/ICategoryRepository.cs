db4e9db57c8a70e3dcd30f456b7127cd4e082b9f
Eslam Mousa <EslamMousa123@gmail.com>
2026-01-06 09:31:27 +0200

    CategoryRepository

---
+++ E-Commerce.Solution/E-Commerce.Core/Domain/RepositoryContracts/ICategoryRepository.cs
@@ -0,0 +1,17 @@
+﻿using E_Commerce.Core.Domain.Entities;
+
+namespace E_Commerce.Core.Domain.RepositoryContracts
+{
+    public interface ICategoryRepository
+    {
+         Task<Category> AddCategory(Category category);
+
+         Task<Category> UpdateCategory(Category category);
+
+         Task<bool> DeleteCategoryById(Guid categoryId );
+
+         Task<Category> GetCategoryById(Guid categoryId);
+
+         Task<IEnumerable<Category>> GetAllCategories();
+    }
+}
---
+++ E-Commerce.Solution/E-Commerce.Infrastructure/Repositories/CategoryRepository.cs
@@ -0,0 +1,53 @@
+﻿using E_Commerce.Core.Domain.Entities;
+using E_Commerce.Core.Domain.RepositoryContracts;
+using E_Commerce.Infrastructure.Data.DBContext;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_Commerce.Infrastructure.Repositories
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public async Task<Category> AddCategory(Category category)
+        {
+            _db.categories.Add(category);
+            await _db.SaveChangesAsync();
+            return category;
+        }
+
+        public async Task<bool> DeleteCategoryById(Guid categoryId)
+        {
+            _db.categories.RemoveRange(_db.categories.Where(temp => temp.Id == categoryId));
+            int rowsDeleted = await _db.SaveChangesAsync();
+            return rowsDeleted > 0;
+
+        }
+
+        public async Task<IEnumerable<Category>> GetAllCategories()
+        {
+            return await _db.categories.ToListAsync();
+        }
+
+        public async Task<Category> GetCategoryById(Guid categoryId)
+        {
+          return await _db.categories.FirstOrDefaultAsync(x=>x.Id == categoryId);
+        }
+
+        public async Task<Category> UpdateCategory(Category category)
+        {
+            var existingCategory = await GetCategoryById(category.Id);
+
+            existingCategory.Name = category.Name;
+            existingCategory.Description = category.Description;
+
+            await _db.SaveChangesAsync();
+            return existingCategory;
+
+        }
+    }
+}
